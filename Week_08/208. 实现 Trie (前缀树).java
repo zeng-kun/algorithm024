e85648e2a4489c6a76ec5d9a0967030e72c2a09e
zengk <1044075504@qq.com>
2021-03-29 13:39:05 +0800

    第8周作业

--- Week_08/208. 实现 Trie (前缀树).java
+++ Week_08/208. 实现 Trie (前缀树).java
@@ -14,20 +14,16 @@ class Trie {
     public void insert(String word) {
         
         char[] chs = word.toCharArray();
-        // 表示从根节点开始向下构建
         int p = 0;
         for (int i = 0; i < chs.length; i++) {
             
             int u = chs[i] - 'a';
-            // 判断p的子节点集合中是否已经存在了chs[i], 不存在则创建
             if (son[p][u] == 0) 
                 son[p][u] = ++idx;
-            // 继续向下构建
             p = son[p][u];
         
         }
 
-        // 以节点p结尾的字符串个数+1
         cnt[p]++;
 
     }
@@ -35,18 +31,14 @@ class Trie {
     public boolean search(String word) {
         
         char[] chs = word.toCharArray();
-        // 从根节点开始向下进行构建
         int p = 0;
         for (int i = 0; i < chs.length; i++) {
             int u = chs[i] - 'a';
-            // 判断p的子节点集合中是否已经存在了chs[i], 不存在则返回false
             if (son[p][u] == 0)
                 return false;
-            // 继续向下构建
             p = son[p][u];
         }
 
-        // 是否存储以节点p结尾的字符串
         return cnt[p] > 0;
 
     }
@@ -54,18 +46,14 @@ class Trie {
     public boolean startsWith(String prefix) {
 
         char[] chs = prefix.toCharArray();
-        // 从根节点开始向下进行构建
         int p = 0;
         for (int i = 0; i < chs.length; i++) {
             int u = chs[i] - 'a';
-            // 判断p的子节点集合中是否已经存在了chs[i], 不存在则返回false
             if (son[p][u] == 0)
                 return false;
-            // 继续向下构建
             p = son[p][u];
         }
 
-        // 存在以prefix为前缀的字符串
         return true;
 
     }
@@ -75,15 +63,11 @@ class Trie {
 
 class Trie {
 
-    // 构造Trie树节点
     class TrieNode {
 
-        // 标识该节点是否是字符串的结束节点
         boolean isEnd = false;
-        // 当前节点的孩子节点
         TrieNode[] next = new TrieNode[26];
 
-        // 设置当前节点为一个字符串的结束节点
         public void setIsEnd (boolean isEnd) {
             this.isEnd = isEnd;
         }
@@ -113,7 +97,6 @@ class Trie {
         
         }
 
-        // 当前TrieNode节点是一个字符串的结尾
         node.setIsEnd(true);
 
     }
@@ -121,12 +104,10 @@ class Trie {
     public boolean search(String word) {
         
         char[] chs = word.toCharArray();
-        // 表示从根节点开始向下构建
         TrieNode node = root;
         for (int i = 0; i < chs.length; i++) {
             
             int u = chs[i] - 'a';
-            // 判断node的子节点集合中是否已经存在了chs[i], 不存在则创建
             if (node.next[u] == null) 
                 return false;
             // 继续向下构建
@@ -134,7 +115,6 @@ class Trie {
         
         }
 
-        // 当前TrieNode节点是否一个字符串的结尾
         return node.isEnd;
 
     }
@@ -147,7 +127,6 @@ class Trie {
         for (int i = 0; i < chs.length; i++) {
             
             int u = chs[i] - 'a';
-            // 判断node的子节点集合中是否已经存在了chs[i], 不存在则创建
             if (node.next[u] == null) 
                 return false;
             // 继续向下构建
